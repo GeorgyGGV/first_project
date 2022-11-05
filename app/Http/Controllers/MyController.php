637544bc1b14b4e448b5b33f75f2582a4ccfd01c
GeorgyGGVe <patriotge@mail.ru>
2022-11-05 13:08:28 +0300

    Chernovik

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\myDatabase;
+
+
+class MyController extends Controller{
+
+ public function submit(Request $req){
+
+ $validation = $req->validate([
+    'name' => 'required|max:15',
+    'email' => 'required|max:50|email:rfc,dns',
+    'message' => 'required|max:150',
+ ]);
+
+$myDatabase = new myDatabase();
+$myDatabase->name = $req ->input('name');
+$myDatabase->email = $req ->input('email');
+$myDatabase->message = $req ->input('message');
+
+$myDatabase->save();
+return redirect()->route("first_project")->with('success', 'База данных пополнилась');
+
+}
+    
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css">
+    <title>First_Project</title>
+</head>
+<body>
+   @yield('contents') 
+</body>
+</html>
